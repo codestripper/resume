c7167a2650ea8ebf84fd1fb675618632aa85ea00
codestripper <brentgerstenschlager@gmail.com>
2021-07-21 22:26:53 -0400

    started on some basic authentication

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using resume.Models;
@@ -26,6 +30,33 @@ namespace resume.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [Authorize]
+        public IActionResult Secured() {
+            return View();
+        }
+
+        [HttpGet("login")]
+        public IActionResult Login(string returnUrl) {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Validate(string username, string password, string returnUrl) {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (username == "codestripper" && password == "toast") {
+                var claims = new List<Claim>();
+                claims.Add(new Claim("username", username));
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+                await HttpContext.SignInAsync(claimsPrincipal);
+                return Redirect(returnUrl);
+            }
+            TempData["Error"] = "Error, Username or Password is incorrect!";
+            return View("login");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]

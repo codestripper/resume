dfad4ea9d92bba4c6791efb2339f6229ab9419b9
codestripper <brentgerstenschlager@gmail.com>
2021-07-29 21:36:50 -0400

    used ef core to scaffold adventureworks

---
+++ Models/AdventureWorks/PersonCreditCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace resume.Models.AdventureWorks
+{
+    public partial class PersonCreditCard
+    {
+        public int BusinessEntityId { get; set; }
+        public int CreditCardId { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual Person BusinessEntity { get; set; }
+    }
+}
